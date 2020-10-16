402cc59248c48d4b77b8c788e17d08fdd0eef2d0
ilianachakarova <iliana1212@abv.bg>
2020-10-16 16:45:55 +0200

    updated Heroku config

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/springblog-angular'));
+app.use(express.static(__dirname + '/springboot-blog-api'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/springblog-angular/index.html'));
+res.sendFile(path.join(__dirname+'/springboot-blog-api/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
