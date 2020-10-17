4e7694223039f0fbeb93197cfd92dedbe573f724
ilianachakarova <iliana1212@abv.bg>
2020-10-17 23:59:36 +0200

    config updates

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.get('/*', function(req,res) {
  
 res.sendFile(path.join(__dirname+'/springboot-blog-api/index.html'));
 });
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
  
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8000);
