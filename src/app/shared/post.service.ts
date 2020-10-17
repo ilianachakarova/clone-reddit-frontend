94de31d41bc089e4fba4dde374380c54aba51a46
ilianachakarova <iliana1212@abv.bg>
2020-10-18 00:37:28 +0200

    magic

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { PostModel } from './post-model';
 import { Observable } from 'rxjs';
 import { CreatePostPayload } from '../post/create-post/create-post.payload';
+
+let headersReq = new HttpHeaders({
+  'Content-Type': 'application/json; charset=utf-8',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET',
+  'Access-Control-Allow-Origin': '*'
+});
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +19,7 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   getAllPosts(): Observable<Array<PostModel>> {
-    return this.http.get<Array<PostModel>>('https://springboot-blog-api.herokuapp.com/api/posts/');
+    return this.http.get<Array<PostModel>>('https://springboot-blog-api.herokuapp.com/api/posts/',{headers: headersReq});
   }
 
   createPost(postPayload: CreatePostPayload): Observable<any> {
@@ -20,10 +27,10 @@ export class PostService {
   }
 
   getPost(id: number): Observable<PostModel> {
-    return this.http.get<PostModel>('https://springboot-blog-api.herokuapp.com/api/posts/' + id);
+    return this.http.get<PostModel>('https://springboot-blog-api.herokuapp.com/api/posts/' + id, {headers: headersReq});
   }
 
   getAllPostsByUser(name: string): Observable<PostModel[]> {
-    return this.http.get<PostModel[]>('https://springboot-blog-api.herokuapp.com/api/posts/by-user/' + name);
+    return this.http.get<PostModel[]>('https://springboot-blog-api.herokuapp.com/api/posts/by-user/' + name, {headers: headersReq});
   }
 }
--- src/app/subreddit/subreddit.service.ts
+++ src/app/subreddit/subreddit.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { SubredditModel } from './subreddit-response';
 import { Observable } from 'rxjs';
 
+let headersReq = new HttpHeaders({
+  'Content-Type': 'application/json; charset=utf-8',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET',
+  'Access-Control-Allow-Origin': '*'
+});
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +17,7 @@ export class SubredditService {
   constructor(private http: HttpClient) { }
 
   getAllSubreddits(): Observable<Array<SubredditModel>> {
-    return this.http.get<Array<SubredditModel>>('https://springboot-blog-api.herokuapp.com/api/subreddit');
+    return this.http.get<Array<SubredditModel>>('https://springboot-blog-api.herokuapp.com/api/subreddit', {headers: headersReq});
   }
 
   createSubreddit(subredditModel: SubredditModel): Observable<SubredditModel> {
@@ -20,6 +26,6 @@ export class SubredditService {
   }
 
   getSubredditById(id:number):Observable<SubredditModel>{
-    return this.http.get<SubredditModel>('https://springboot-blog-api.herokuapp.com/api/subreddit/' + id);
+    return this.http.get<SubredditModel>('https://springboot-blog-api.herokuapp.com/api/subreddit/' + id, {headers: headersReq});
   }
 }
